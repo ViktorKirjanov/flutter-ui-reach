c3cf1013217cc56ae987f8dbe332011c575ccdfb
Viktor <viktor.kirjanov@hashcoins.com>
2020-05-17 16:41:29 +0300

    portrait only

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:login_and_dashboard/core/blocs/selected_chart_bar_bloc/selected_chart_bar_bloc.dart';
 import 'package:login_and_dashboard/pages/init_page/init_page.dart';
 
-void main() => runApp(
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+      .then((_) {
+    runApp(
       MultiBlocProvider(
         providers: [
           BlocProvider<SelectedChartBarBloc>(
@@ -13,12 +18,15 @@ void main() => runApp(
         child: MyApp(),
       ),
     );
+  });
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      // debugShowCheckedModeBanner: false,
+      title: 'Reach',
       theme: ThemeData(
         fontFamily: 'Inter',
         primaryColor: Color.fromRGBO(224, 222, 234, 1),
